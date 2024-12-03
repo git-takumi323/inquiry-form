39ddd637f1375c39ebfbcf7a25ec23f64df36406
git-takumi <kirin3235525@gmail.com>
2024-12-04 08:54:53 +0900

    model create

---
+++ src/app/Models/Category.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * テーブル名
+     */
+    protected $table = 'users';
+
+    /**
+     * マスアサイン可能な属性
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * パスワードの隠蔽
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * リレーション: Contacts
+     */
+    public function contacts()
+    {
+        return $this->hasMany(Contact::class);
+    }
+}
---
+++ src/app/Models/Contact.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    use HasFactory;
+    /**
+     * テーブル名
+     */
+    protected $table = 'contacts';
+
+    /**
+     * マスアサイン可能な属性
+     */
+    protected $fillable = [
+        'category_id',
+        'first_name',
+        'last_name',
+        'gender',
+        'email',
+        'tel',
+        'address',
+        'building',
+        'detail',
+    ];
+    /**
+     * リレーション: Category
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * リレーション: User
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
